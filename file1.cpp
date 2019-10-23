7d1d204a8411e0beb90ccbfe6482a2df8914f993
Somif <sabdlli7880f@example.com>
2019-10-23 19:09:00 +0330

    Edit start func

--- file1.cpp
+++ file1.cpp
@@ -30,37 +30,12 @@ struct Class
 };
 
 vector <Class> Database;
-string currentclass ;
-
-void Start()
-{
-	vector <string> cmdline(4) ;
-	cmdline.resize(10);
-	string str ;
-	string temp ;
-	getline(cin,str) ;
-
-	for( auto i : str )
-	{
-		if( i != ' ')
-		{
-			temp+=i ;
-		}else
-		{
-			cmdline.push_back(temp) ;
-			cout<< temp ;
-			temp=' ';
-		}
-	}
-	cout<<cmdline.size();
-	cout<<endl ;
-	cout<<cmdline.at(0) ;
-}
-
-//*********************************************************
+string currentclass ; 
+//******************************************************
 void SelectClass(string newclass)
 {
  	string currentclass = newclass ;
+	 cout << endl << currentclass ;
 }
 //*********************************************************
 void AddClass(string filename)
@@ -104,11 +79,38 @@ void AddClass(string filename)
 //*********************************************************
 void RemoveClass(string nameclass)
 {
+	for(Class i : Database)
+	{
+		if(i.ClassName == currentclass)
+		{
+			
+		}
+	}
 }
 //********************************************************* 
 void AddStudent(string flname, Date birthday, unsigned long long int id, float gray)
 {
-
+	Student newstudent ;
+	string date ;
+	string temp[3] ;
+	size_t count = 0 ;
+	newstudent.Firstname ;
+	newstudent.Lastname ;
+	newstudent.ID == id ;
+	newstudent.Grade == gray ;
+	for ( auto i : date )
+	{
+		if  (i == '/' )
+			count ++ ;
+		else
+		{
+			temp[count] += i ;
+		}
+	}
+	newstudent.Birthday.Day   =  stoi(temp[0]) ;
+	newstudent.Birthday.Month =  stoi(temp[1]) ;
+	newstudent.Birthday.Year  =  stoi(temp[2]) ;
+	//Data.push_back(newstudent) ;
 }
 //********************************************************* 
 void RemoveStudent(unsigned long long int id)
@@ -128,9 +130,39 @@ void RemoveStudent(unsigned long long int id)
 	}
 }
 //********************************************************* 
-void Search(unsigned long long int);
+void Search(unsigned long long int id) 
+{
+	for (auto i : Database)
+	{
+		for (size_t j=0 ; j < i.Capacity ; j++)
+			{
+				if (i.Data.at(j).ID == id)
+				{
+					cout << i.Data.at(j).Firstname ;
+					cout << i.Data.at(j).Lastname  ;
+					cout << i.Data.at(j).Grade ;
+					cout << i.Data.at(j).Birthday.Day << "/" << i.Data.at(j).Birthday.Month << "/" << i.Data.at(j).Birthday.Year ;
+				}
+			}
+	}
+	
+}
 //********************************************************* 
-void Search(string, string);
+void Search( string Name , string Familyname )
+{
+	for (auto i : Database)
+	{
+		for (size_t j=0 ; j < i.Capacity ; j++)
+			{
+				if (i.Data.at(j).Firstname == Name && i.Data.at(j).Lastname == Familyname)
+				{
+					cout << i.Data.at(j).ID  ;
+					cout << i.Data.at(j).Grade ;
+					cout << i.Data.at(j).Birthday.Day << "/" << i.Data.at(j).Birthday.Month << "/" << i.Data.at(j).Birthday.Year ;
+				}
+			}
+	}
+}
 //********************************************************* 
 void ShowClass(string nameclass)
 {
@@ -145,6 +177,98 @@ void SortByID();
 //********************************************************* 
 void Save();
 //********************************************************* 
+void Start()
+{
+	vector <string> cmdline ;
+	string str ;
+	string temp ; 
+	do{
+		getline(cin,str) ;
+		for( auto i : str )
+		{
+			
+			if( i != ' ')
+			{
+				temp += i ;
+			}else
+			{
+				cmdline.push_back(temp) ;
+				cout << temp ;
+				temp = "" ;
+			
+			}
+		}
+		size_t t=0 ;
+		/*for (auto i : cmdline)
+		{
+			cout << cmdline.at(t) ;
+			t++ ;
+		}
+		*/
+		cout << endl ;
+		cout << cmdline.at(1) ;
+		if ( cmdline.at(1) == "select" && cmdline.at(2) == "class" )
+		{
+			cout<< 6666 ;
+			cout<< cmdline[t+=3] ;
+			SelectClass( cmdline.at(3));
+		}
+				
+		if ( cmdline.at(1) == "add" && cmdline.at(2) == "class" )
+		{
+			
+		}
+
+		if ( cmdline.at(1) == "remove" &&  cmdline.at(2) == "class"  )
+		{
+			
+		}
+
+		
+		if ( cmdline.at(1)=="select" && cmdline.at(2)=="class" && cmdline.at(3)=="none")
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="remove" && cmdline.at(2)=="student" )
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="search" )
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="show") 
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="sort" && cmdline.at(2)=="name" )
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="sort" && cmdline.at(2)=="id" )
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="save" )
+		{
+
+		}
+		
+		if ( cmdline.at(1)=="help" )
+		{
+
+		}
+
+	} while ( cmdline.at(0) == "exit") ;
+	
+}
+//******************************************************
 int main()
  {
 	Start()  ; 
